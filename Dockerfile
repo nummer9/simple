9b3875b6735d429d6fc17ef0746d250518262aa4
nummer4 <rambo.ramon.de@gmail.com>
2018-03-02 19:11:53 +0100

    minor changes

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ FROM ubuntu:17.10
 RUN apt-get update && apt-get install -y \
 curl
 
-COPY ../../../simple/simple /usr/local/bin/simple
+COPY ./simple /usr/local/bin/simple
 
 CMD ["simple"]
--- handlers/healthHandler.go
+++ handlers/healthHandler.go
@@ -12,5 +12,5 @@ func (rcv HealthHandler) ServeHTTP(rw http.ResponseWriter, rq *http.Request) {
 	fmt.Println("received web-request to /health")
 
 	rw.WriteHeader(http.StatusOK)
-	rw.Write([]byte("simple health check\n"))
+	rw.Write([]byte("simple health check: success \n"))
 }
