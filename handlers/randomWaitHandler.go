9596b166ff38848e4e09ce7e2ab8659a6cb6bdb5
BloodyRainer <rambo.ramon.de@gmail.com>
2019-03-13 16:23:32 +0100

    added response string to random-wait

--- handlers/randomWaitHandler.go
+++ handlers/randomWaitHandler.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"bytes"
+	"io"
 	"math/rand"
 	"net/http"
 	"strconv"
@@ -30,6 +32,9 @@ func (rcv RandomWaitHandler) ServeHTTP(w http.ResponseWriter, rq *http.Request) 
 
 	log.Info("received web-request to /random-wait, waited " + strconv.Itoa(wait) + " ms")
 
+	buff := new(bytes.Buffer)
+	buff.WriteString("waited " + strconv.Itoa(wait) + " ms")
+	io.Copy(w, buff)
 	w.WriteHeader(http.StatusOK)
 
 }
